e2b834e6f96e1dbb9760841b119fc67444545dd2
MiryalaAkshitha <81464051+MiryalaAkshitha@users.noreply.github.com>
2021-04-12 23:12:00 +0530

    final:update
    doggie translation using javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate= document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/doge.json"
+
+function errorHandler(error){
+    alert("Something went wrong. Try again later..!");
+}
+
+function addQueryToURL(inputText){
+    return serverURL + "?"+ "text="+ inputText
+}
+
+function clickHandler() {
+    
+    var text = txtInput.value;
+
+        fetch(addQueryToURL(text))
+        .then(response => response.json() )
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+        }  
+
+
+btnTranslate.addEventListener("click", clickHandler);
